ef345aaa0856781d39f1f578e80c8c0eff850057
SapphicBear <sleepyscreenart@gmail.com>
2026-03-27 17:39:28 +0100

    Finish adding dummy data to database populater.
    Add some dummy data so that the database can be fired up with some
    default values for the tables.

--- db/populatedb.js
+++ db/populatedb.js
@@ -8,12 +8,12 @@ CREATE TABLE IF NOT EXISTS game_consoles (
     console_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
     company VARCHAR(100),
-    release_year INTERVAL YEAR NOT NULL
+    release_year VARCHAR(10)
 );
 CREATE TABLE IF NOT EXISTS game_studios (
     studio_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(200),
-    year INTERVAL YEAR NOT NULL
+    year VARCHAR(10)
 );
 CREATE TABLE IF NOT EXISTS games (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -21,25 +21,30 @@ CREATE TABLE IF NOT EXISTS games (
     studio_id INTEGER NOT NULL REFERENCES game_studios(studio_id),
     console_id  INTEGER NOT NULL REFERENCES game_consoles(console_id),
     genre VARCHAR(255),
-    release_year INTERVAL YEAR NOT NULL
+    release_year VARCHAR(10) 
 );
 INSERT INTO game_consoles (name, company, release_year)
-    VALUES ('Xbox 360', 'Microsoft', '2005');
-INSERT INTO game_consoles (name, company, release_year)
-    VALUES ('PlayStation 2', 'Sony', '2000');
-INSERT INTO game_consoles (name, company, release_year)
-    VALUES ('PlayStation', 'Sony', '1994');
-INSERT INTO game_consoles (name, company, release_year)
-    VALUES ('PlayStation 3', 'Sony', '2006');
-INSERT INTO game_consoles (name, company, release_year)
-    VALUES ('PC', 'Multiple Companies', '1979');
+    VALUES 
+        ('Xbox 360', 'Microsoft', '2005'), 
+        ('PlayStation 2', 'Sony', '2000'), 
+        ('PlayStation', 'Sony', '1994'), 
+        ('PlayStation 3', 'Sony', '2006'), 
+        ('PC', 'Multiple Companies', '1979'),
+        ('All Platforms', 'Multiple Companies', null);
 
 INSERT INTO game_studios (name, year)
-    VALUES ('Valve', '1996');
-INSERT INTO game_studios (name, year)
-    VALUES ('Obsidian Entertainment', '2003');
-INSERT INTO game_studios (name, year)
-    VALUES ('Black Isle Studios', '1996');
+    VALUES ('Valve', '1996'), 
+    ('Obsidian Entertainment', '2003'), 
+    ('Black Isle Studios', '1996');
+
+INSERT INTO games (name, studio_id, console_id, genre, release_year)
+    VALUES (
+        'Fallout: New Vegas', 
+        (SELECT studio_id FROM game_studios WHERE name = 'Obsidian Entertainment'),
+        (SELECT console_id FROM game_consoles WHERE name = 'All Platforms'),
+        'RPG',
+        '2010'
+        );
 `;
 
 async function main(arg) {
